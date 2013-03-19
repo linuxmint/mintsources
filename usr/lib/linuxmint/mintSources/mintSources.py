61b89df71fd113c3aad48f144512374b82a14230
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-03-19 13:07:33 +0000

    More i18n

--- usr/lib/linuxmint/mintSources/mintSources.py
+++ usr/lib/linuxmint/mintSources/mintSources.py
@@ -318,7 +318,12 @@ class Application(object):
             else:
                 self.config[section] = {}                        
                 for param in config_parser.options(section):                
-                    self.config[section][param] = config_parser.get(section, param)          
+                    self.config[section][param] = config_parser.get(section, param)     
+
+        self.builder.get_object("label_title_official").set_markup("%s" % _("Official repositories"))     
+        self.builder.get_object("label_title_ppa").set_markup("%s" % _("PPAs"))     
+        self.builder.get_object("label_title_3rd").set_markup("%s" % _("Additional repositories"))     
+        self.builder.get_object("label_title_keys").set_markup("%s" % _("Authentication keys"))     
 
         self.builder.get_object("label_mirrors").set_markup("<b>%s</b>" % _("Mirrors"))    
         self.builder.get_object("label_mirror_description").set_markup("%s (%s)" % (_("Main"), self.config["general"]["codename"]) )
